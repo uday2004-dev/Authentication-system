901017c4fb223ebaed4144ea0818f4afc6177e0b
Uday Shrivastav <udayshrivastav2004@gmail.com>
2026-03-19 13:37:45 +0530

    set up for authentication system

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import cookieParser from "cookie-parser"
+
+
+const app=express()
+
+
+const port=process.env.PORT || 4000
+
+app.use(express.json())
+
+app.use(cookieParser())
+app.use(cors({credentials:true}))
+
+
+app.get("/",(req,res)=>{
+    res.send("You are on Home page")
+})
+
+
+app.listen(port ,()=>{
+    console.log(`Server is running on port ${port}`)
+})
