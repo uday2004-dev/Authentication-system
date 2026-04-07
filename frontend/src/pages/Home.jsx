84bd2de9de9cb7ffd4d20de6d03e8ed82979cc8e
Uday Shrivastav <udayshrivastav2004@gmail.com>
2026-04-07 23:42:37 +0530

    makes component and pages file folder and basic set up done

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../componets/NavBar'
+import Header from '../componets/Header'
+
+const Home = () => {
+  return (
+    <div>
+        <NavBar/>
+        <Header/>
+    </div>
+  )
+}
+
+export default Home
