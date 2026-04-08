7c5cb8645d893272dbc22a5e727be6dfe143a2e9
Uday Shrivastav <udayshrivastav2004@gmail.com>
2026-04-08 17:57:49 +0530

    login page created

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from '../componets/Header'
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
         <NavBar/>
         <Header/>
     </div>
